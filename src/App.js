54189a0cfe33aa1fff9d41034a429ff419a5d9e8
Michael <natanpopov6138@gmail.com>
2021-10-01 15:55:13 +0300

    Update1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import {Component} from "react";
+import axios from "axios";
 
 class App extends Component{
+  constructor() {
+    super();
+
+    this.state = {
+      wordLocal: 'Word local',
+      wordRemote: null
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://62.109.2.137:8080/helloDeployment')
+      .then(response => {
+        this.setState({
+          wordRemote: response.data
+        })
+      })
+
+  }
 
   render() {
     return (
       <div>
-        Hi React
+        <div>{this.state.wordLocal}</div>
+        <div>{this.state.wordRemote}</div>
       </div>
 
     );
